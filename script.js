6b3076973c3b387ec63ecc7eb0971f4f6ad76130
NDoroteya93 <doroteanikolova93@gmail.com>
2016-12-09 13:38:32 +0200

    Javascript homework.

---
+++ script.js
@@ -0,0 +1,78 @@
+// Array Literals
+
+var proteins = ["Gold Standard", "Pure Whey", "Syntha-6"];
+
+// Boolean Literals
+
+var liers = false;
+var happy = true;
+
+// Integers Literals 
+// Decimal 
+var a = 0;
+var	b = 117;
+var	c = -375;
+// Octal
+var d = 015;
+var	e = 0001;
+var	f = -0o77;
+// Hexadecimal
+var g = 0x1123;
+var	h = 0x00111;
+var	i = -0xF1A7;
+// Binary
+var j = 0b11;
+var	k = 0b0011;
+var	l = -0b11;
+// Floating points literals
+var m = 3.1415926;
+var	n = - .12345678; 
+var	o = -2.1E+12; 
+var	p = .1e-23;
+
+// Objects Literals
+// var sales = "Toyota";
+
+// function carTypes(name) {
+// 	if (name === "Honda") {
+// 		return name;
+// 	} else {
+// 		return "Sorry, we don't sell" + name + ".";
+// 	}
+// }
+
+// var car = {myCar: "Saturn", getCar: carTypes("Honda"), special: sales;}
+
+// console.log(car.myCar); //Saturn
+// console.log(car.getCar);//Honda
+// console.log(car.special); //Toyota
+
+///////////////////////////////////
+// var obj = {
+// 	//_proto_
+// 	_proto_:theProtoObj,
+// 	// Shortland for 'handler:handler'
+// 	handler, 
+// 	// Methods
+// 	return "d " + super.toString();
+// };
+// Computed (dynamic) property names
+// ['prop_' + (() => 42)()]:42
+
+// String Literals
+var r = "dori";
+var	s = 'bar';
+var	t = '1234';
+var	u = "one line \n another line"
+// console.log('John`s cat'.length);
+// Wiil print the number of symbols in the string including whitespace
+// In this case, 10
+
+
+// Escaping characters
+var quote = "He read\"The Cremation of sam McGee\" by R.W. Service.";
+// console.log(quote);
+
+
+// Typeof Null
+var emprtyString = null;
